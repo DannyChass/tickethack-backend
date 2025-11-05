2f652b1ca542501795b171bd0af9db6e02ab11f3
Daniel Chassagnez Goncalves <daniel.chassagn@gmail.com>
2025-11-05 09:58:05 +0100

    add login route

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,47 @@ router.post('/addUser', async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
   }
+});
+
+// POST - Ajouter un trip au panier d'un utilisateur
+router.post('/addTripToUserCart', async (req, res) => {
+  const { userId, tripId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+
+    const Trip = require('../models/trip');
+    const trip = await Trip.findById(tripId);
+    if (!trip) {
+      return res.status(404).json({ error: 'Voyage non trouvé' });
+    }
+
+    user.cart.push(trip._id);
+
+    await user.save();
+
+    res.json({ message: 'Voyage ajouté au panier', cart: user.cart });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email, password: req.body.password })
+    if (user) {
+      res.json({ sucess: true, result: user });
+    } else {
+      res.json({ sucess: false })
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
 })
 
 module.exports = router;
